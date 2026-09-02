6c344eb59636b02930b44aaac4f78f33297b056e
y-kobayashi71 <y-kobayashi@fusic.co.jp>
2026-09-02 14:07:28 +0900

    feat: db導入

---
+++ src/main/db/index.ts
@@ -0,0 +1,12 @@
+import { app } from 'electron'
+import { join } from 'path'
+import Database from 'better-sqlite3'
+import { drizzle } from 'drizzle-orm/better-sqlite3'
+import * as schema from './schema'
+
+const dbPath = join(app.getPath('userData'), 'mementolane.db')
+
+const sqlite = new Database(dbPath)
+sqlite.pragma('journal_mode = WAL')
+
+export const db = drizzle(sqlite, { schema })
