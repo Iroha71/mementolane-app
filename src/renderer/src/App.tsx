612fb2ecb9196bdc32f49e6f40f92470f3d4e384
iroha <y-kobayashi@fusic.co.jp>
2026-09-05 09:37:41 +0900

    feat: AppにはRouterを配置してコンポーネントベースで画面を表示するように変更 #7

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,56 +1,7 @@
-import StatusLane from './components/StatusLane'
-import { useEffect, useState } from 'react'
-import { TaskOutput } from '@shared/task'
-import { ArrowRightFromLine, Construction, MessageSquareCode, Package, Pencil } from 'lucide-react'
-
-const STATUSES = [
-  {
-    name: 'plan',
-    variant: '',
-    icon: <Pencil />
-  },
-  {
-    name: 'thisweek',
-    variant: '',
-    icon: <ArrowRightFromLine />
-  },
-  {
-    name: 'wip',
-    variant: '',
-    icon: <Construction />
-  },
-  {
-    name: 'inreview',
-    variant: '',
-    icon: <MessageSquareCode />
-  },
-  {
-    name: 'inspection',
-    variant: '',
-    icon: <Package />
-  }
-] as const
+import AppRoutes from './route'
 
 function App(): React.JSX.Element {
-  const [tasks, setTasks] = useState<TaskOutput[]>([])
-  useEffect(() => {
-    window.api.readActiveTasks().then(setTasks)
-  }, [])
-
-  return (
-    <div className="flex h-screen w-screen gap-3 overflow-hidden p-4 pb-28">
-      {STATUSES.map((status) => (
-        <StatusLane
-          color="primary"
-          tasks={tasks}
-          key={status.name}
-          status={status.name}
-          icon={status.icon}
-          className="min-w-0 flex-1"
-        ></StatusLane>
-      ))}
-    </div>
-  )
+  return <AppRoutes />
 }
 
 export default App
